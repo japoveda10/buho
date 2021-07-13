1d231271b3ae47f25fb2fa3f3fd2df6d172eb097
japoveda10 <ja.poveda10@uniandes.edu.co>
2021-07-12 22:27:34 -0500

    Added splash screen

---
+++ app/src/main/java/com/educationalappsdev/buho/ui/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.educationalappsdev.buho.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.educationalappsdev.buho.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
